86c74fccfc9e371b63db12fe99acd9263f820d1d
muhammadnowshad <muhammadnowshad2020@gmail.com>
2023-06-13 08:46:12 +0600

    cors,mongoose,express,parser setup

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import config from './config/index'
+import app from "./app";
+
+async function main() {
+    
+    try {
+
+        await mongoose.connect(config.database_url as string);
+        console.log('database connected');
+
+        app.listen(config.port, () => {
+            console.log(`Application is listening on port ${config.port}`)
+        })
+
+    } catch (error) {
+        console.log('Failed to connected database', error)
+    }
+
+};
+
+main();
